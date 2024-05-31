ef6079f228083fb45ce18898344b96be7ea19bd4
fbhood <mail@fabiopacifici.com>
2024-05-31 13:13:31 +0200

    add latest route

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,18 +3,27 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Post;
 
 class PostController extends Controller
 {
-
     public function index()
     {
         $posts = Post::with('category', 'tags')->orderByDesc('id')->paginate(12);
+
         return response()->json([
             'success' => true,
-            'posts' => $posts
+            'posts' => $posts,
+        ]);
+    }
+
+    public function latest()
+    {
+        $posts = Post::with('category', 'tags')->orderByDesc('id')->take(4)->get();
+
+        return response()->json([
+            'success' => true,
+            'posts' => $posts,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\API\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::get('posts', [PostController::class, 'index']);
+Route::get('latest', [PostController::class, 'latest']);
